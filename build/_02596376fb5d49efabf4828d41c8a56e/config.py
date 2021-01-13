b718b1ad2563e3ffac11bea49f88616cd8cf477c
苍之幻灵 <www.cpcgskill.com>
2021-01-13 17:57:45 +0800

    编译原始程序

---
+++ build/_02596376fb5d49efabf4828d41c8a56e/config.py
@@ -0,0 +1,15 @@
+
+import sys
+import _02596376fb5d49efabf4828d41c8a56e
+_02596376fb5d49efabf4828d41c8a56e.config = sys.modules.get(__name__)
+u'\n:\u521b\u5efa\u65f6\u95f4: 2020/12/30 20:32\n:\u4f5c\u8005: \u82cd\u4e4b\u5e7b\u7075\n:\u6211\u7684\u4e3b\u9875: https://cpcgskill.com\n:QQ: 2921251087\n:\u7231\u53d1\u7535: https://afdian.net/@Phantom_of_the_Cang\n:aboutcg: https://www.aboutcg.org/teacher/54335\n:bilibili: https://space.bilibili.com/351598127\n\n'
+import os
+PATH = os.path.dirname(os.path.abspath(__file__)).replace(u'\\', u'/')
+DEBUG = False
+Title = u'Tools'
+START_TIME = u'2020'
+Version = 0.1
+ASSETS = (PATH + u'/assets')
+IMAGES = (PATH + u'/assets/images')
+HEAD_IMG = (PATH + u'/assets/images/head.png')
+del os
